9575f4a53018bcb5fbb82745a6643f761707b6c1
Michal Sudwoj <msudwoj@student.ethz.ch>
2016-09-09 15:08:23 +0200

    Font converter script (requires fontforge)

---
+++ fontconvert.sh
@@ -0,0 +1,29 @@
+#!/usr/local/bin/fontforge
+# https://fontforge.github.io/en-US/documentation/scripting/
+i=1
+format=".ttf"
+while ( i<$argc )
+  if ( $argv[i]=="-format" || $argv[i]=="--format" )
+    i=i+1
+    if ( i<$argc )
+      format = $argv[i]
+      if ( format!=".ttf" && format!=".otf" && \
+      format!=".pfb" && format!=".svg" )
+    Error( "Expected one of '.ttf', '.otf', '.pfb' or '.svg' for format" )
+      endif
+    endif
+  else
+    Open($argv[i])
+    if ( $order==2 && (format==".otf" || format==".pfb" ))
+      SetFontOrder(3)
+      SelectAll()
+      Simplify(128+32+8,1.5)
+      ScaleToEm(1000)
+    elseif ( $order==3 && format==".ttf" )
+      ScaleToEm(2048)
+      RoundToInt()
+    endif
+    Generate($argv[i]:r + format)
+  endif
+  i = i+1
+endloop
